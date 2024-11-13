aeb4bf4a94183b63d190d7f023e7f6c734d4b25d
Tommy Ly <tommyly201@gmail.com>
2024-11-13 14:36:41 -0500

    try this

---
+++ prepare_data.py
@@ -0,0 +1,11 @@
+import multiprocessing
+
+
+def main():
+    # Your existing main code here
+    ...
+
+
+if __name__ == "__main__":
+    multiprocessing.freeze_support()  # Optional, needed only if you plan to create executables
+    main()
