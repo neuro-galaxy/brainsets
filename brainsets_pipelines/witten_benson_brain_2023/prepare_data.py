2fa90cf8fd63de625a5ceb55c4ba608e3b736a47
Tommy Ly <tommyly201@gmail.com>
2024-11-26 17:25:45 -0500

    add witten_benson files

---
+++ brainsets_pipelines/witten_benson_brain_2023/prepare_data.py
@@ -0,0 +1,385 @@
+"""Load data, process it, and save it."""
+
+import sys
+import argparse
+import h5py
+import numpy as np
+import pandas as pd
+import os
+from datetime import datetime
+import logging
+from brainsets import serialize_fn_map  # This contains the serialization functions
+
+from one.api import ONE
+from ibl_data_utils import (
+    prepare_data,
+    select_brain_regions,
+    list_brain_regions,
+    bin_spiking_data,
+    bin_behaviors,
+    align_spike_behavior,
+    load_target_behavior,
+    load_anytime_behaviors,
+)
+
+from temporaldata import (
+    Data,
+    IrregularTimeSeries,
+    Interval,
+    # DatasetBuilder,
+    ArrayDict,
+)
+from brainsets.taxonomy import (
+    Task,
+    Sex,
+    Species,
+    SubjectDescription,
+    SessionDescription,
+    DeviceDescription,
+    BrainsetDescription,
+    RecordingTech,
+)
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Set random seed for reproducibility
+np.random.seed(42)
+
+# -------
+# SET UP
+# -------
+ap = argparse.ArgumentParser(description="Process neural and behavioral data.")
+ap.add_argument(
+    "--eid",
+    type=str,
+    default="c7bf2d49-4937-4597-b307-9f39cb1c7b16",
+    help="Experiment ID",
+)
+ap.add_argument(
+    "--base_path", type=str, default="./processed", help="Base path for output files"
+)
+args = ap.parse_args()
+
+base_path = args.base_path
+eid = args.eid
+
+logging.info(f"Processing session: {eid}")
+
+params = {
+    "interval_len": 2,
+    "binsize": 0.02,
+    "single_region": False,
+    "align_time": "stimOn_times",
+    "time_window": (-0.5, 1.5),
+    "fr_thresh": 0.5,
+}
+
+# Initialize ONE
+one = ONE(
+    base_url="https://openalyx.internationalbrainlab.org",
+    password="international",
+)
+
+# Load metadata
+bwm_df = pd.read_csv("2023_12_bwm_release.csv", index_col=0)
+
+
+# ---------
+# Load Data
+# ---------
+neural_dict, _, meta_data, trials_data, _ = prepare_data(
+    one, eid, bwm_df, params, n_workers=1
+)
+
+regions, beryl_reg = list_brain_regions(neural_dict, **params)
+region_cluster_ids = select_brain_regions(neural_dict, beryl_reg, regions, **params)
+binned_spikes, clusters_used_in_bins = bin_spiking_data(
+    region_cluster_ids,
+    neural_dict,
+    trials_df=trials_data["trials_df"],
+    n_workers=1,
+    **params,
+)
+
+# Filter out inactive neurons
+avg_fr = binned_spikes.sum(1).mean(0) / params["interval_len"]
+active_neuron_ids = np.argwhere(avg_fr > 1 / params["fr_thresh"]).flatten()
+
+
+# -------------------------
+# Extract Spiking Activity
+# -------------------------
+logging.info("Extracting spikes ...")
+
+spike_times = neural_dict["spike_times"]
+spike_clusters = neural_dict["spike_clusters"]
+unit_mask = np.isin(spike_clusters, active_neuron_ids)
+spike_times = spike_times[unit_mask]
+spike_clusters = spike_clusters[unit_mask]
+unit_ids = np.array(meta_data["uuids"])[active_neuron_ids]
+
+# Convert to spike data object
+unit_meta, timestamps, unit_index = [], [], []
+for i in range(len(unit_ids)):
+    unit_id = unit_ids[i]
+    times = spike_times[spike_clusters == i]
+    timestamps.append(times)
+
+    if len(times) > 0:
+        unit_index.extend([i] * len(times))
+
+    unit_meta.append(
+        {
+            "id": unit_id,
+            "unit_number": i,
+            "count": len(times),
+            "type": 0,
+        }
+    )
+
+unit_meta_df = pd.DataFrame(unit_meta)
+units = ArrayDict.from_dataframe(
+    unit_meta_df,
+    unsigned_to_long=True,
+)
+timestamps = np.concatenate(timestamps)
+unit_index = np.array(unit_index)
+spikes = IrregularTimeSeries(
+    timestamps=timestamps,
+    unit_index=unit_index,
+    domain="auto",
+)
+spikes.sort()
+
+# Convert unit IDs to string
+units.id = units.id.astype(str)
+
+
+# ---------------
+# Extract Trials
+# ---------------
+logging.info("Extracting trials ...")
+"""
+Sync trials mask.
+"""
+trial_mask = trials_data["trials_mask"]
+binned_spikes, clusters_used_in_bins = bin_spiking_data(
+    region_cluster_ids,
+    neural_dict,
+    trials_df=trials_data["trials_df"],
+    n_workers=1,
+    **params,
+)
+binned_behaviors, behavior_masks = bin_behaviors(
+    one, eid, trials_df=trials_data["trials_df"], allow_nans=True, n_workers=1, **params
+)
+aligned_binned_spikes, aligned_binned_behaviors, target_mask, del_idxs = (
+    align_spike_behavior(binned_spikes, binned_behaviors, trials_data["trials_mask"])
+)
+trial_mask = np.array(target_mask).astype(bool).tolist()
+trials_data["trials_df"] = trials_data["trials_df"][trial_mask]
+
+start_time = trials_data["trials_df"][params["align_time"]] + params["time_window"][0]
+end_time = trials_data["trials_df"][params["align_time"]] + params["time_window"][1]
+
+max_num_trials = sum(trial_mask)
+trial_idxs = np.random.choice(np.arange(max_num_trials), max_num_trials, replace=False)
+train_idxs = trial_idxs[: int(0.7 * max_num_trials)]
+val_idxs = trial_idxs[int(0.7 * max_num_trials) : int(0.8 * max_num_trials)]
+test_idxs = trial_idxs[int(0.8 * max_num_trials) :]
+trial_split = np.array(["train"] * max_num_trials)
+trial_split[val_idxs] = "val"
+trial_split[test_idxs] = "test"
+
+# Convert to trials data object
+trial_table = pd.DataFrame(
+    {
+        "start": start_time,
+        "end": end_time,
+        "split_indicator": trial_split,
+    }
+)
+trials = Interval.from_dataframe(trial_table)
+
+train_mask_nwb = trial_table.split_indicator.to_numpy() == "train"
+val_mask_nwb = trial_table.split_indicator.to_numpy() == "val"
+test_mask_nwb = trial_table.split_indicator.to_numpy() == "test"
+
+trials.train_mask_nwb = train_mask_nwb
+trials.val_mask_nwb = val_mask_nwb
+trials.test_mask_nwb = test_mask_nwb
+
+
+# -----------------
+# Extract Behaviors
+# -----------------
+logging.info("Extracting behaviors ...")
+
+behave_dict = load_anytime_behaviors(one, eid, n_workers=1)
+
+# Extract wheel speed
+wh_timestamps = behave_dict["wheel-speed"]["times"]
+wh_values = behave_dict["wheel-speed"]["values"].reshape(-1, 1)
+behavior_type = np.ones_like(wh_timestamps, dtype=np.int64) * 0
+eval_mask = np.zeros_like(wh_timestamps, dtype=bool)
+for i in range(len(trials)):
+    eval_mask[(wh_timestamps >= trials.start[i]) & (wh_timestamps < trials.end[i])] = (
+        True
+    )
+wheel = IrregularTimeSeries(
+    timestamps=wh_timestamps,
+    values=wh_values,
+    subtask_index=behavior_type,
+    eval_mask=eval_mask,
+    domain=Interval(trials.start[0], trials.end[-1]),
+)
+
+# Extract whisker
+try:
+    me_timestamps = behave_dict["left-whisker-motion-energy"]["times"]
+    me_values = behave_dict["left-whisker-motion-energy"]["values"].reshape(-1, 1)
+except KeyError:
+    me_timestamps = behave_dict["right-whisker-motion-energy"]["times"]
+    me_values = behave_dict["right-whisker-motion-energy"]["values"].reshape(-1, 1)
+behavior_type = np.ones_like(me_timestamps, dtype=np.int64) * 0
+eval_mask = np.zeros_like(me_timestamps, dtype=bool)
+for i in range(len(trials)):
+    eval_mask[(me_timestamps >= trials.start[i]) & (me_timestamps < trials.end[i])] = (
+        True
+    )
+whisker = IrregularTimeSeries(
+    timestamps=me_timestamps,
+    values=me_values,
+    subtask_index=behavior_type,
+    eval_mask=eval_mask,
+    domain=Interval(trials.start[0], trials.end[-1]),
+)
+
+
+# Extract choice
+def map_choice(data):
+    choice_map = {"-1": 0, "1": 1}
+    return choice_map.get(str(int(data)), -1)  # Default to -1 if not found
+
+
+choice = trials_data["trials_df"].choice
+
+stim_start_times = start_time.to_numpy()
+stim_end_times = end_time.to_numpy()
+
+# Create Interval for choice
+choice = Interval(
+    start=stim_start_times,
+    end=stim_end_times,
+    choice=choice.apply(map_choice).to_numpy(),
+    timestamps=(stim_start_times + stim_end_times) / 2.0,
+    timekeys=["start", "end", "timestamps"],
+)
+
+
+# Extract block
+def map_block(data):
+    block_map = {"0.2": 0, "0.5": 1, "0.8": 2}
+    return block_map.get(str(data), -1)  # Default to -1 if not found
+
+
+block = trials_data["trials_df"].probabilityLeft
+
+# Create Interval for block
+block = Interval(
+    start=stim_start_times,
+    end=stim_end_times,
+    block=block.apply(map_block).to_numpy(),
+    timestamps=(stim_start_times + stim_end_times) / 2.0,
+    timekeys=["start", "end", "timestamps"],
+)
+
+# Extract reward
+reward = (trials_data["trials_df"]["rewardVolume"] > 1).astype(int).to_numpy()
+# Create Interval for reward
+reward = Interval(
+    start=stim_start_times,
+    end=stim_end_times,
+    reward=reward,
+    timestamps=(stim_start_times + stim_end_times) / 2.0,
+    timekeys=["start", "end", "timestamps"],
+)
+
+# -----------------
+# Save Data Object
+# -----------------
+logging.info("Saving data ...")
+
+# Create metadata descriptions
+brainset_description = BrainsetDescription(
+    id=f"ibl_{eid}",
+    origin_version="2023_12",
+    derived_version="1.0.0",
+    source="https://openalyx.internationalbrainlab.org",
+    description="IBL mouse behavioral and neural data during decision-making task",
+)
+
+subject = SubjectDescription(
+    id=one.get_details(eid)["subject"],
+    species=Species.from_string("MUS_MUSCULUS"),
+    sex=Sex.from_string("MALE"),
+)
+
+session_description = SessionDescription(
+    id=eid,
+    recording_date=datetime.today(),
+    task=Task.FREE_BEHAVIOR,
+)
+
+device_description = DeviceDescription(
+    id=f"{subject.id}_{eid}",
+    recording_tech=RecordingTech.NEUROPIXELS_SPIKES,
+)
+
+# Register session start and end
+session_start, session_end = (
+    trials.start[0],
+    trials.end[-1],
+)
+
+units.id = units.id.astype(str)
+
+# Create the final Data object
+data = Data(
+    brainset=brainset_description,
+    subject=subject,
+    session=session_description,
+    device=device_description,
+    spikes=spikes,
+    units=units,
+    trials=trials,
+    wheel=wheel,
+    whisker=whisker,
+    choice=choice,
+    block=block,
+    reward=reward,
+    domain=Interval(session_start, session_end),
+)
+
+# Split trials into train, validation, and test sets
+train_trials = trials.select_by_mask(trials.train_mask_nwb)
+valid_trials = trials.select_by_mask(trials.val_mask_nwb)
+test_trials = trials.select_by_mask(trials.test_mask_nwb)
+
+# Create train sampling intervals by excluding dilated valid and test regions
+train_sampling_intervals = data.domain.difference(
+    (valid_trials | test_trials).dilate(3.0)
+)
+
+# Set the domains in the data object
+data.set_train_domain(train_sampling_intervals)
+data.set_valid_domain(valid_trials)
+data.set_test_domain(test_trials)
+
+# Save data to disk
+output_path = os.path.join(base_path, f"ibl_{eid}.h5")
+os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
+with h5py.File(output_path, "w") as file:
+    data.to_hdf5(file, serialize_fn_map=serialize_fn_map)
