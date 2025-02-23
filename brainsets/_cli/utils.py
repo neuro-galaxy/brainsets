d78eb7e7b7d367b64bf164bfef9a6c90af39f389
Vinam Arora <vinam@posteo.de>
2025-02-22 20:07:38 -0500

    Converts config file to yaml

--- brainsets/_cli/utils.py
+++ brainsets/_cli/utils.py
@@ -1,8 +1,8 @@
-import json
+import yaml
 from pathlib import Path
 import brainsets_pipelines
 
-CONFIG_FILE = Path.home() / ".brainsets_config.json"
+CONFIG_FILE = Path.home() / ".brainsets.yaml"
 PIPELINES_PATH = Path(brainsets_pipelines.__path__[0])
 DATASETS = [d.name for d in PIPELINES_PATH.iterdir() if d.is_dir()]
 
@@ -10,10 +10,10 @@ DATASETS = [d.name for d in PIPELINES_PATH.iterdir() if d.is_dir()]
 def load_config():
     if CONFIG_FILE.exists():
         with open(CONFIG_FILE, "r") as f:
-            return json.load(f)
+            return yaml.safe_load(f)
     return {"raw_dir": None, "processed_dir": None}
 
 
 def save_config(config):
     with open(CONFIG_FILE, "w") as f:
-        json.dump(config, f, indent=2)
+        yaml.safe_dump(config, f, default_flow_style=False)
