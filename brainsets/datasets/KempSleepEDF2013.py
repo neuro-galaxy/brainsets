679577ba464d04e4a54eafc6a147bac35eb82b05
Vinam Arora <vinam@seas.upenn.edu>
2025-12-20 21:31:27 -0500

    Add KempSleepEDF2013 Dataset class

---
+++ brainsets/datasets/KempSleepEDF2013.py
@@ -0,0 +1,64 @@
+from dataclasses import dataclass
+from typing import Callable, Optional, Literal
+from pathlib import Path
+from torch_brain.utils import np_string_prefix
+from temporaldata import Data
+
+from torch_brain.dataset import Dataset
+
+
+class KempSleepEDF2013(Dataset):
+    def __init__(
+        self,
+        root: str,
+        dirname: str = "perich_miller_population_2018",
+        recording_ids: Optional[list[str]] = None,
+        transform: Optional[Callable] = None,
+        uniquify_channel_ids: bool = True,
+        split_type: Optional[Literal["fold_0", "fold_1", "fold_2"]] = "fold_0",
+        **kwargs,
+    ):
+        super().__init__(
+            dataset_dir=Path(root) / dirname,
+            recording_ids=recording_ids,
+            transform=transform,
+            namespace_attributes=["session.id", "subject.id", "units.id"],
+            **kwargs,
+        )
+
+        self.uniquify_channel_ids = uniquify_channel_ids
+        self.split_type = split_type
+
+    def get_sampling_intervals(
+        self,
+        split: Optional[Literal["train", "valid", "test"]] = None,
+    ):
+
+        if self.split_type == None:
+            # No split type specified => user wants full domain
+            return {rid: self.get_recording(rid).domain for rid in self.recording_ids}
+
+        if self.split_type not in ["fold_0", "fold_1", "fold_2"]:
+            raise ValueError(
+                f"Invalid split_type '{self.split_type}'."
+                " Must be one of ['fold_0', 'fold_1', 'fold_2'] or None."
+            )
+
+        if split not in ["train", "valid", "test"]:
+            raise ValueError(
+                f"Invalid split '{split}'. Must be one of ['train', 'valid', 'test']."
+            )
+
+        key = f"{self.split_type}.{split}"
+        return {
+            rid: self.get_recording(rid).get_nested_attribute(key)
+            for rid in self.recording_ids
+        }
+
+    def get_recording_hook(self, data: Data):
+        # This dataset does not have unique channel ids across sessions
+        # so we prefix the unit ids with the session id to ensure uniqueness
+        if self.uniquify_channel_ids:
+            data.channels.id = np_string_prefix(f"{data.session.id}/", data.channels.id)
+
+        super().get_recording_hook(data)
