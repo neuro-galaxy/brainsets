b42c1feb6cc9aa7eddc750e5f028a5db8c6d7ee6
Vinam Arora <vinam@seas.upenn.edu>
2025-12-22 23:14:32 -0500

    version is "unknown" when package not found

--- brainsets/__init__.py
+++ brainsets/__init__.py
@@ -6,4 +6,4 @@ try:
     __version__ = version("brainsets")
 except PackageNotFoundError:  # pragma: no cover
     # This can happen if someone is importing brainsets without installing
-    pass  # pragma: no cover
+    __version__ = "unknown"  # pragma: no cover
