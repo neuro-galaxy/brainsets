55ecea897e3bd1c86e5459d63d0345a022fd83c7
Oumayma Gharbi <oumayma.gharbi@umontreal.ca>
2026-01-19 17:05:41 -0500

    Placeholders for download and process methods

--- brainsets_pipelines/kelly_CMI_developingbrain_2016/pipeline.py
+++ brainsets_pipelines/kelly_CMI_developingbrain_2016/pipeline.py
@@ -64,10 +64,9 @@ class Pipeline(BrainsetPipeline):
                     and rel_parts[1] == "EEG"
                     and rel_parts[2] == "raw"
                     and rel_parts[3] == "raw_format"
-                ):
+                ) and filename.endswith(".raw"):
                     participant_id = rel_parts[0]
-                    session_id = filename.replace(".", "_")
-                    session_id = f"eeg_{session_id}"
+                    session_id = f"{Path(filename).stem}"
 
                     manifest_rows.append(
                         {
@@ -79,3 +78,45 @@ class Pipeline(BrainsetPipeline):
 
         manifest = pd.DataFrame(manifest_rows).set_index("session_id")
         return manifest
+
+    def download(self, manifest_item) -> Path:
+        self.update_status("DOWNLOADING")
+        s3 = get_s3_client_for_download()
+
+        s3_key = manifest_item.s3_key
+
+        local_path = self.raw_dir / Path(s3_key).relative_to(self.prefix)
+        local_path.parent.mkdir(parents=True, exist_ok=True)
+
+        if not local_path.exists() or self.args.redownload:
+            logging.info(f"Downloading: {Path(s3_key).name}")
+            s3.download_file(self.bucket, s3_key, str(local_path))
+        else:
+            logging.info(f"Skipping download, file exists: {local_path}")
+
+        return local_path
+
+    def process(self, download_output: Path) -> None:
+        raw_path = download_output
+
+        self.update_status("PROCESSING")
+
+        base_name = raw_path.stem
+
+        output_path = self.processed_dir / "EEG" / f"{base_name}.h5"
+        if output_path.exists() and not self.args.reprocess:
+            self.update_status("Skipped Processing")
+            logging.info(f"Skipping processing, file exists: {output_path}")
+            return
+
+        logging.info(f"Processing file: {raw_path}")
+
+        self.update_status("Reading raw file")
+
+        output_path.parent.mkdir(parents=True, exist_ok=True)
+
+        with h5py.File(output_path, "w") as file:
+            file.attrs["raw_file"] = str(raw_path)
+            file.attrs["brainset_id"] = self.brainset_id
+
+        logging.info(f"Saved processed data to: {output_path}")
