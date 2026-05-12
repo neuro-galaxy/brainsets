f2e1b26b0a47cf1bffc74d781ce944a5bcf082a7
estefanysuarez <laura.suarez@mail.mcgill.ca>
2026-05-12 10:29:33 -0400

    Fixed typo in ShiraziHBNR1DS005505 Dataset class

--- brainsets/datasets/ShiraziHBNR1DS005505.py
+++ brainsets/datasets/ShiraziHBNR1DS005505.py
@@ -35,7 +35,7 @@ class ShiraziHBNR1DS005505(OpenNeuroDataset):
         transform: Optional[Callable] = None,
         **kwargs,
     ):
-        dataset_dir = "shirazi_hbn_r1_ds005505"
+        dataset_dir = "shirazi_hbnr1_ds005505"
         super().__init__(
             root=root,
             dataset_dir=dataset_dir,
